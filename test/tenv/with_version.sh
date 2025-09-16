d19b71d51e8260251a17e4441eac56dfb1b85b95
Sean Summers <165854+seansummers@users.noreply.github.com>
2025-09-16 11:29:17 +0000

    troubleshoot with_version

--- test/tenv/with_version.sh
+++ test/tenv/with_version.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "tenv version | grep 'tenv version 4.7.21'"
+check "execute command" bash -c "tenv version | grep 'tenv version '"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
