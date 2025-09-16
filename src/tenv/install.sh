6d5a97ab29b47387b69921535400f0a916edc91a
Sean Summers <165854+seansummers@users.noreply.github.com>
2025-09-16 11:25:28 +0000

    continue the de-bashification

--- src/tenv/install.sh
+++ src/tenv/install.sh
@@ -10,7 +10,8 @@ FILTER=".assets[].browser_download_url | select(match(\"_${ARCH}.deb$\"))"
 echo "Activating feature 'tenv' from ${REPO}"
 echo "The provided version is: ${VERSION}"
 
-if [ "${VERSION}" == "latest" ]; then
+if test "${VERSION}" = "latest"
+then
   VERSION=$(curl -sSL https://api.github.com/repos/${REPO}/releases/latest | jq -r .tag_name)
   echo "Resolved 'latest' to: ${VERSION}"
 fi
